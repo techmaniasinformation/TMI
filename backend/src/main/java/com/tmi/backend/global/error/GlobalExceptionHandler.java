f72b76403f832ebeda1b70bb011a28f97a585a64
eunseongman <99581773+eunseongman@users.noreply.github.com>
2025-07-25 11:56:40 +0900

    [S13P11A509-31] <chore> errorcode 확립 - 기본적으로 사용할 errorcode 를 작성하였습니다.

--- backend/src/main/java/com/tmi/backend/global/error/GlobalExceptionHandler.java
+++ backend/src/main/java/com/tmi/backend/global/error/GlobalExceptionHandler.java
@@ -37,7 +37,7 @@ public class GlobalExceptionHandler {
     Map<String, Object> data = new HashMap<>();
     data.put("errors", errors);
     return ApiErrorResponse.error(
-        INVALID_PARAMETER.getCode(), data, INVALID_PARAMETER.getMessage());
+        COMMON_INVALID_FORMAT.getCode(), data, COMMON_INVALID_FORMAT.getMessage());
   }
 
   @ExceptionHandler(value = BusinessException.class)

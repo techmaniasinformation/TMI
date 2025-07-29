bedefc8bd3638be18a580582ef39668bc23d4d86
DESKTOP-RBNO6LS\SSAFY <dudhr17050@naver.com>
2025-07-29 19:02:11 +0900

    [S13P11A509-60] <chore> Security, Jwt, OAuth 의존성 추가 및 SecurityConfig 생성 - Security, Jwt, OAuth 의존성을 추가합니다. - SecurityConfig를 생성합니다.

---
+++ backend/src/main/java/com/tmi/backend/global/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.tmi.backend.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  private final JwtFilter jwtFilter;
+  private final OAuth2SuccessHandler oAuth2SuccessHandler;
+  private final CustomOAuth2UserService customOAuth2UserService;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth
+            //TODO: 경로 추가하기
+            .requestMatchers("/auth/**", "/login/**", "/oauth2/**", "/signup/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        .oauth2Login(oauth -> oauth
+            .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+            .successHandler(oAuth2SuccessHandler)
+        );
+
+    http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}

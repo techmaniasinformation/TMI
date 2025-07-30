172a9931d362b3dd39ec82f13f88ad8373389892
DESKTOP-RBNO6LS\SSAFY <dudhr17050@naver.com>
2025-07-30 15:31:19 +0900

    [S13P11A509-160] <feat> Spring Security의 UserDetails 구현 - Security의 UserDetails를 구현합니다. - Security의 UserDetailsService를 구현합니다.

---
+++ backend/src/main/java/com/tmi/backend/global/auth/CustomUserDetails.java
@@ -0,0 +1,57 @@
+package com.tmi.backend.global.auth;
+
+import com.tmi.backend.domain.member.entity.Member;
+import java.util.Collection;
+import java.util.List;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+  private final Long memberId;
+  private final Collection<? extends GrantedAuthority> authorities;
+
+  public CustomUserDetails(Member member) {
+    this.memberId = member.getId();
+    // 모든 사용자에게 ROLE_USER만
+    this.authorities = List.of(() -> "ROLE_USER");
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorities;
+  }
+
+  // JWT 인증이라 비밀번호 불필요
+  @Override
+  public String getPassword() {
+    return "";
+  }
+
+  @Override
+  public String getUsername() {
+    return memberId.toString();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
---
+++ backend/src/main/java/com/tmi/backend/global/auth/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.tmi.backend.global.auth;
+
+import com.tmi.backend.domain.member.repository.MemberRepository;
+import com.tmi.backend.global.error.ErrorCode;
+import com.tmi.backend.global.error.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+  private final MemberRepository memberRepository;
+
+  //username = Spring Security가 내부적으로 인가(Authorization) 등을 위해 조회하는 “사용자 이름” 역할
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Long memberId = Long.valueOf(username);
+    return memberRepository.findById(memberId)
+        .map(CustomUserDetails::new)
+        .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+  }
+}

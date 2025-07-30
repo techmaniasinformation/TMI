064b1efcfc84a5c157e4a9dc6308b0965013076a
eunseongman <99581773+eunseongman@users.noreply.github.com>
2025-07-30 14:07:36 +0900

    [S13P11A509-147] <feat> 스타 조회 및 삭제 API 개발 - 해당 멤버의 스타 목록을 조회하는 API 개발 - 해당 스타를 삭제하는 API 개발

--- backend/src/main/java/com/tmi/backend/domain/star/controller/StarController.java
+++ backend/src/main/java/com/tmi/backend/domain/star/controller/StarController.java
@@ -1,15 +1,24 @@
 package com.tmi.backend.domain.star.controller;
 
 import com.tmi.backend.domain.star.dto.request.StarRegisterRequest;
+import com.tmi.backend.domain.star.dto.response.SimpleStarResponse;
+import com.tmi.backend.domain.star.dto.response.StarListResponse;
 import com.tmi.backend.domain.star.service.StarService;
 import com.tmi.backend.global.common.response.ApiResponse;
+import com.tmi.backend.global.common.response.impl.ApiErrorResponse;
 import com.tmi.backend.global.common.response.impl.ApiSuccessResponse;
+import com.tmi.backend.global.error.ErrorCode;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,11 +29,38 @@ public class StarController {
   private final StarService starService;
 
   // TODO : 권한 검증 필요
+
+  /**
+   * 스타 등록 API
+   * @param request 멤버 id 와 게시글 id 를 받습니다.
+   */
   @PostMapping
   public ApiResponse<?> starRegister(
       @Valid @RequestBody StarRegisterRequest request
   ) {
 
     return ApiSuccessResponse.success(starService.register(request.memberId(), request.postId()));
+  }
+
+  /**
+   * 멤버 스타 등록 조회 API
+   * @param memberId 원하는 사용자의 id
+   * @return 스타 id 와 게시글 id 를 목록으로 반환합니다.
+   */
+  @GetMapping
+  public ApiResponse<StarListResponse> getStarList(@Positive @RequestParam Long memberId) {
+
+    return ApiSuccessResponse.success(starService.readStars(memberId));
+  }
+
+  /**
+   * 스타 취소 API
+   * @param starId 취소하려는 스타 id
+   */
+  @DeleteMapping("/{starId}")
+  public ApiResponse<?> deleteStar(@PathVariable Long starId) {
+
+    starService.deleteStar(starId);
+    return ApiSuccessResponse.success();
   }
 }
--- backend/src/main/java/com/tmi/backend/domain/star/service/StarService.java
+++ backend/src/main/java/com/tmi/backend/domain/star/service/StarService.java
@@ -4,10 +4,13 @@ import com.tmi.backend.domain.member.entity.Member;
 import com.tmi.backend.domain.member.repository.MemberRepository;
 import com.tmi.backend.domain.post.entity.Post;
 import com.tmi.backend.domain.post.repository.PostRepository;
+import com.tmi.backend.domain.star.dto.response.StarListResponse;
 import com.tmi.backend.domain.star.entity.Star;
 import com.tmi.backend.domain.star.repository.StarRepository;
 import com.tmi.backend.global.error.ErrorCode;
 import com.tmi.backend.global.error.exception.BusinessException;
+import jakarta.validation.constraints.Positive;
+import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,8 +34,11 @@ public class StarService {
     Member member = memberRepository.findById(memberId)
         .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
 
+    log.info("postId = {}", postId);
     Post post = postRepository.findById(postId)
         .orElseThrow(() -> new BusinessException(ErrorCode.POST_NOT_FOUND));
+    log.info("post title = {}", post.getTitle());
+
 
     if (starRepository.existsByMemberAndPost(member, post)) {
       throw new BusinessException(ErrorCode.STAR_ALREADY_STARRED);
@@ -41,5 +47,25 @@ public class StarService {
     Star star = starRepository.save(Star.of(member, post));
 
     return Map.of("starId", star.getId());
+  }
+
+  public StarListResponse readStars(Long memberId) {
+    log.info("StarService : readStars(" + memberId + ") 호출");
+
+    Member member = memberRepository.findById(memberId)
+        .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+    List<Star> memberStars = starRepository.findByMember(member);
+
+    return StarListResponse.from(memberStars);
+  }
+
+  @Transactional
+  public void deleteStar(Long starId) {
+
+    Star star = starRepository.findById(starId)
+        .orElseThrow(() -> new BusinessException(ErrorCode.STAR_NOT_FOUND));
+
+    starRepository.delete(star);
   }
 }

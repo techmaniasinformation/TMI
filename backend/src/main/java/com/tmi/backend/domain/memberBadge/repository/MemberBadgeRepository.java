a20bf22246aade3f96522193ddccc89622cfe55d
DESKTOP-RBNO6LS\SSAFY <dudhr17050@naver.com>
2025-08-05 21:49:35 +0900

    [S13P11A509-172] <fix> 멤버 삭제시 멤버뱃지 삭제 기능 구현 - 멤버 삭제시 멤버뱃지 삭제 기능을 구현합니다.

--- backend/src/main/java/com/tmi/backend/domain/memberBadge/repository/MemberBadgeRepository.java
+++ backend/src/main/java/com/tmi/backend/domain/memberBadge/repository/MemberBadgeRepository.java
@@ -1,18 +1,45 @@
 package com.tmi.backend.domain.memberBadge.repository;
 
+import com.tmi.backend.domain.memberBadge.dto.response.SimpleMemberBadge;
 import com.tmi.backend.domain.memberBadge.entity.MemberBadge;
-import java.util.Optional;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface MemberBadgeRepository extends JpaRepository<MemberBadge, Long> {
 
   @Query("""
-        SELECT mb.badge.badgeUrl
-          FROM MemberBadge mb
-         WHERE mb.member.id = :memberId
-           AND mb.isRepresentative = true
+        SELECT new com.tmi.backend.domain.memberBadge.dto.response.SimpleMemberBadge(
+          mb.id,
+          mb.badge.id,
+          mb.receivedAt
+        )
+        FROM MemberBadge mb
+        WHERE mb.member.id = :memberId
+        ORDER BY mb.receivedAt DESC
       """)
-  Optional<String> findRepresentativeBadgeUrlByMemberId(@Param("memberId") Long memberId);
+  List<SimpleMemberBadge> findAllSimpleByMemberId(
+      @Param("memberId") Long memberId
+  );
+
+  @Modifying(clearAutomatically = true)
+  @Query("""
+        UPDATE MemberBadge mb
+           SET mb.isRepresentative =
+             CASE WHEN mb.id = :targetId THEN true ELSE false END
+         WHERE mb.member.id = (
+           SELECT m2.member.id
+             FROM MemberBadge m2
+            WHERE m2.id = :targetId
+         )
+      """)
+  int updateRepresentative(@Param("targetId") Long targetId);
+
+  @Modifying
+  @Query("DELETE FROM MemberBadge mb WHERE mb.member.id = :memberId")
+  void deleteAllByMemberId(@Param("memberId") Long memberId);
+
 }
+

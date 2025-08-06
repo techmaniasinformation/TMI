cf6ccc96ee0c953b73054203af07d66014f9ad46
DESKTOP-RBNO6LS\SSAFY <dudhr17050@naver.com>
2025-08-06 14:15:15 +0900

    <fix> 미구현 기능 주석처리 - 미기능 구현 주석처리

--- backend/src/main/java/com/tmi/backend/domain/notification/repository/NotificationRepository.java
+++ backend/src/main/java/com/tmi/backend/domain/notification/repository/NotificationRepository.java
@@ -1,7 +1,6 @@
 package com.tmi.backend.domain.notification.repository;
 
 import com.tmi.backend.domain.notification.entity.Notification;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,31 +8,31 @@ import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-  @Query("""
-          SELECT n
-          FROM Notification n
-          LEFT JOIN FETCH n.member
-          LEFT JOIN FETCH n.senderMember
-          LEFT JOIN FETCH n.senderCompany
-          LEFT JOIN FETCH n.post
-          LEFT JOIN FETCH n.badge
-          WHERE n.member.id = :memberId
-          ORDER BY n.createdAt DESC
-      """)
-  List<Notification> findAllByMemberId(@Param("memberId") Long memberId);
+//  @Query("""
+//          SELECT n
+//          FROM Notification n
+//          LEFT JOIN FETCH n.member
+//          LEFT JOIN FETCH n.senderMember
+//          LEFT JOIN FETCH n.senderCompany
+//          LEFT JOIN FETCH n.post
+//          LEFT JOIN FETCH n.badge
+//          WHERE n.member.id = :memberId
+//          ORDER BY n.createdAt DESC
+//      """)
+//  List<Notification> findAllByMemberId(@Param("memberId") Long memberId);
 
-  @Query("""
-          SELECT n
-          FROM Notification n
-          LEFT JOIN FETCH n.member
-          LEFT JOIN FETCH n.senderMember
-          LEFT JOIN FETCH n.senderCompany
-          LEFT JOIN FETCH n.post
-          LEFT JOIN FETCH n.badge
-          WHERE n.member.id = :memberId AND n.isRead = false
-          ORDER BY n.createdAt DESC
-      """)
-  List<Notification> findUnreadByMemberId(@Param("memberId") Long memberId);
+//  @Query("""
+//          SELECT n
+//          FROM Notification n
+//          LEFT JOIN FETCH n.member
+//          LEFT JOIN FETCH n.senderMember
+//          LEFT JOIN FETCH n.senderCompany
+//          LEFT JOIN FETCH n.post
+//          LEFT JOIN FETCH n.badge
+//          WHERE n.member.id = :memberId AND n.isRead = false
+//          ORDER BY n.createdAt DESC
+//      """)
+//  List<Notification> findUnreadByMemberId(@Param("memberId") Long memberId);
 
   @Modifying
   @Query("DELETE FROM Notification n WHERE n.member.id = :memberId")

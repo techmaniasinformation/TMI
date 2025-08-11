2b1e4ab39dc62ffbd961ad18c69d7c9931aee911
SeoYoonmi <sym2005sym@naver.com>
2025-08-11 18:56:52 +0900

    <fix> 로그인시 메시지 오류 해결

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -14,14 +14,19 @@ import SignupPage from '../pages/SignupPage';
 import OAuthCallbackPage from '../pages/OAuthCallbackPage';
 import { ROUTES } from './routes';
 import { useUserStore } from '@/stores/userStore';
+import { useLocation } from 'react-router-dom';
 
 // 로그인 페이지 접근 확인 위함.
 function LoginRouteGuard() {
-  const { isLogin, memberId } = useUserStore();
+  const location = useLocation();  
+  const { memberId } = useUserStore();
 
-  console.log('[LoginRouteGuard] isLogin:', isLogin, 'memberId:', memberId);
+  console.log('[LoginRouteGuard]memberId:', memberId);
+  const searchParams = new URLSearchParams(location.search);  // &&& 수정: location.search 사용
+  const isNew = searchParams.get('isNew');
+  const queryMemberId = searchParams.get('memberId');
 
-  if (memberId !== -1) {
+  if (memberId !== -1 && !(isNew === 'false' && queryMemberId)) {
     console.log('[LoginRouteGuard] 이미 로그인 상태이므로 홈으로 리다이렉트');
     alert('이미 로그인 되어 있습니다');
     return <Navigate to={ROUTES.HOME} replace />;

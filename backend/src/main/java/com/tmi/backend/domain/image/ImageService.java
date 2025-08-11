a54fa90819a3174c1883af231e3fe19988d8dba1
소태호 <sth9057@gmail.com>
2025-08-11 15:43:28 +0900

    <refactor> URL에 저장되는 값 수정 - URL만 넣으면 바로 호출할 수 있도록 완전한 URL을 넣는 것으로 변경하였습니다. - open in view 옵션을 껐습니다. - 이미지를 반환하는 API를 작성하였습니다.

---
+++ backend/src/main/java/com/tmi/backend/domain/image/ImageService.java
@@ -0,0 +1,32 @@
+package com.tmi.backend.domain.image;
+
+import com.tmi.backend.global.Utils.FileUtil;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ImageService {
+
+  private final FileUtil fileUtil;
+
+  public ResponseEntity<byte[]> getImage(String filename, String domain) throws IOException {
+    byte[] fileBytes = fileUtil.getFile(filename, domain);
+
+    HttpHeaders headers = new HttpHeaders();
+    Path filePath = Paths.get(fileUtil.getUploadDir(), domain, filename);
+    headers.setContentType(MediaType.parseMediaType(Files.probeContentType(filePath)));
+
+    return new ResponseEntity<>(fileBytes, headers, HttpStatus.OK);
+  }
+}

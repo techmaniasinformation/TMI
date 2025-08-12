4aff0b89f04ff36977617b40bce42d8a1bf443a6
SeoYoonmi <sym2005sym@naver.com>
2025-08-12 16:40:49 +0900

    [feat-프론트-라우팅-개선] <fix> 로그아웃 요청방식 수정 POST 요청이 아닌 GET 요청으로 수정

--- frontend/src/components/foundation/Header.tsx
+++ frontend/src/components/foundation/Header.tsx
@@ -61,7 +61,7 @@ const Header: React.FC<HeaderProps> = ({
       const response = await fetch(
         `https://i13a509.p.ssafy.io/api/v1/auth/logout/${user?.memberId}/${socialProvider}`,
         {
-          method: 'POST',
+          method: 'GET',
           credentials: 'include',
         }
       );

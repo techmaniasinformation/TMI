464c9baeb54824484ee5a3fcd6ff67aa1c284f0b
jm13 <ktd562@naver.com>
2025-08-13 16:18:57 +0900

    [S13P11A509]<fix>프로필 수정 이미지 업로드 기능 수정 - 훅 이용하여 이미지 업로드 수정했습니다. - 회원 탈퇴 모달 글 수정했습니다.

--- frontend/src/components/layout/mypage/WithdrawalConfirmModal.tsx
+++ frontend/src/components/layout/mypage/WithdrawalConfirmModal.tsx
@@ -70,9 +70,10 @@ export default function WithdrawalConfirmModal({
         <button onClick={onCancel} className="absolute top-3 right-3 text-gray-500 hover:text-gray-700" disabled={loading} aria-label="닫기">
           &times;
         </button>
-        <div className="pt-4">
-          <p className="text-center text-gray-800 mb-4">
-            탈퇴 후 7일 이내 재가입이 불가합니다. 정말 탈퇴하시겠습니까?
+        <div className="mt-8">
+          <p className="text-center text-gray-800 mb-12">
+            탈퇴 시 모든 정보가 영구 삭제됩니다.<br />
+            진행하시겠습니까?
           </p>
 
           {errorMessage && (
